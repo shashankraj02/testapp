50706135a92ca73fe22fadf09d2c773cdcbd21a7
AK <mailshashankraj@gmail.com>
2017-04-30 23:35:50 +0530

    added webview

--- app/src/main/java/com/raj/shashank/testapp/MainActivity.java
+++ app/src/main/java/com/raj/shashank/testapp/MainActivity.java
@@ -2,12 +2,16 @@ package com.raj.shashank.testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView browser = (WebView) findViewById(R.id.webview);
+
+        browser.loadUrl("http://www.google.com");
     }
 }
