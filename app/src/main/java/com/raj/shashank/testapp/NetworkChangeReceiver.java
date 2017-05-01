28647c6d89b2baf103c790e50dfa511561bf6398
AK <mailshashankraj@gmail.com>
2017-05-01 23:31:08 +0530

    Added Receiver 02

---
+++ app/src/main/java/com/raj/shashank/testapp/NetworkChangeReceiver.java
@@ -0,0 +1,30 @@
+package com.raj.shashank.testapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by ak on 1/5/17.
+ */
+
+public class NetworkChangeReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifi = connMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo mobile = connMgr.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+        boolean isConnected = wifi != null && wifi.isConnectedOrConnecting() ||
+                mobile != null && mobile.isConnectedOrConnecting();
+        if (isConnected) {
+            Log.d("Network Available ", "YES");
+        } else {
+            Log.d("Network Available ", "NO");
+        }
+    }}
